74b8b6778be1bf70e2c11a97d96c6f00bbb34c2f
frank-107 <20253ds107@utez.edu.mx>
2026-01-22 18:51:29 -0600

    chore de iva

--- GitConflictoFuentesEstudio/src/Main.java
+++ GitConflictoFuentesEstudio/src/Main.java
@@ -3,9 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double iva = 0.16;
 
         double subtotal = pedirDouble(scanner, "Subtotal: ");
-        double total = subtotal;
+        double total = subtotal*iva;
 
         System.out.printf("Total a pagar: %.2f%n", total);
     }
